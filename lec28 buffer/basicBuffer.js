9e3b2e733855ad32638168267e1ad5de413427a4
manish8code <8manishgupta8@gmail.com>
2025-10-21 21:57:06 +0530

    buffer completed

---
+++ lec28 buffer/basicBuffer.js
@@ -0,0 +1,30 @@
+const { Buffer } = require("buffer");
+// const buf = Buffer.alloc(4);
+// console.log(buf);
+
+// const buf=Buffer.from("hello manish");
+// // console.log(buf)
+// // console.log(buf.toString())
+
+// const buf2=Buffer.allocUnsafe(115)
+// console.log(buf2)
+
+// const buf=Buffer.alloc(10)
+// buf.write('hello')
+// console.log(buf.toString())
+
+// const buf=Buffer.from('manish gupta')
+// console.log(buf.toString())
+// console.log(buf.toString('utf8',0,6))
+
+// const buf=Buffer.from('manish')
+// buf[0]=0x4A
+// console.log(buf.toString())
+
+//concatination of buffers
+// const buf1=Buffer.from('manish')
+// const buf2=Buffer.from('gupta')
+// const merged=Buffer.concat([buf1,buf2])
+// console.log(merged.toString())
+
+//compare and find the length of buffer
